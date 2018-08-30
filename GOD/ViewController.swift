d6180613067a683bc7bd655a270b070616b3d93b
LakshmiNarayananN <laks@docureach.com>
2018-08-30 10:09:52 +0530

    Initial commit

---
+++ GOD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GOD
+//
+//  Created by Lakshmi Narayanan N on 30/08/18.
+//  Copyright © 2018 Lakshmi Narayanan N. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
